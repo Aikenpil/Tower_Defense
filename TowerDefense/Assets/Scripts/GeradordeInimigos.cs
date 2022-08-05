c48ee52503c0a6408c32b3e643008c62f84e2a0b
Aikenpil <gustebas@hotmail.com>
2022-08-04 21:09:54 -0300

    add enemygenerator

---
+++ TowerDefense/Assets/Scripts/GeradordeInimigos.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradordeInimigos : MonoBehaviour
+{
+
+    [SerializeField] private GameObject inimigo;
+    private float momentoDaUltimaGeracao;
+    [Range(0, 3)]
+    [SerializeField] private float tempoDeCriacao = 2f;
+    void Update()
+    {
+        GeraInimigo();
+    }
+    private void GeraInimigo()
+    {
+        float tempoAtual = Time.time;
+        if (tempoAtual > momentoDaUltimaGeracao + tempoDeCriacao)
+        {
+            momentoDaUltimaGeracao = tempoAtual;
+            Vector3 posicaoDoGerador = this.transform.position;
+            Instantiate(inimigo, posicaoDoGerador,
+            Quaternion.identity);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+}
